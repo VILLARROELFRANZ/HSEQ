cdb990a4d864ddb5b3a20e70bf8a8ac5cff8fc17
VILLARROELFRANZ <villarroelfranz1@gmail.com>
2024-01-08 23:44:44 -0400

    cambios en rama actualizado5

--- login/urls.py
+++ login/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', views.signin, name='signin'),
     path('home/', views.home, name='home'),
     path('signout/', views.signout, name='signout'),
-    path('register_request/', views.register_request, name='register_request'),
-
 ]
